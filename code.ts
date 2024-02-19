170a6d2a11df4025952d02d8b040a0a17a881a49
nu0ma <igreen.re317v10@gmail.com>
2024-02-19 23:29:00 +0900

    feat: remove [] for query

--- code.ts
+++ code.ts
@@ -2,7 +2,7 @@ figma.showUI(__html__, { width: 400, height: 220 });
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === "calculate-sum-for-query-in-selection") {
-    const queryEnclosed = `[${msg.query}]`;
+    const queryEnclosed = `${msg.query}`;
     const regexNumbers = /\[\d+\]/g;
 
     const selectedNodes = figma.currentPage.selection.filter(
