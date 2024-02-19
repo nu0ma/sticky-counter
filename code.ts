d1b56194e9e9f8048dbfea375b980cd89bbe33ef
nu0ma <igreen.re317v10@gmail.com>
2024-02-19 22:38:14 +0900

    fff

--- code.ts
+++ code.ts
@@ -1,24 +1,27 @@
 figma.showUI(__html__, { width: 400, height: 220 });
 
-figma.ui.onmessage = msg => {
-  if (msg.type === 'calculate-sum-for-query-in-selection') {
-    const queryEnclosed = `[${msg.query}]`;
-    const regexNumbers = /\[\d+\]/g;
+figma.ui.onmessage = (msg) => {
+	if (msg.type === "calculate-sum-for-query-in-selection") {
+		const queryEnclosed = `[${msg.query}]`;
+		const regexNumbers = /\[\d+\]/g;
 
-    const selectedNodes = figma.currentPage.selection.filter(node => 
-      node.type === 'STICKY' && node.text.characters.includes(queryEnclosed)) as StickyNode[];
-    let sum = 0;
+		const selectedNodes = figma.currentPage.selection.filter(
+			(node) =>
+				node.type === "STICKY" && node.text.characters.includes(queryEnclosed),
+		) as StickyNode[];
 
-    selectedNodes.forEach(node => {
-      const matches = node.text.characters.match(regexNumbers);
-      if (matches) {
-        matches.forEach((match: string) => {
-          const number = parseInt(match.slice(1, -1), 10); 
-          sum += number;
-        });
-      }
-    });
+		let sum = 0;
 
-    figma.ui.postMessage({ type: 'sum-result', sum });
-  }
+		for (const node of selectedNodes) {
+			const matches = node.text.characters.match(regexNumbers);
+			if (matches) {
+				for (const match of matches) {
+					const number = parseInt(match.slice(1, -1), 10);
+					sum += number;
+				}
+			}
+		}
+
+		figma.ui.postMessage({ type: "sum-result", sum });
+	}
 };
