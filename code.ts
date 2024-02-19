a2f58fdb6002f84261b56f87867d37bb89a0a4eb
nu0ma <igreen.re317v10@gmail.com>
2024-02-19 22:19:01 +0900

    good

--- code.ts
+++ code.ts
@@ -1,33 +1,24 @@
-figma.showUI(__html__, { width: 240, height: 140 });
+figma.showUI(__html__, { width: 400, height: 220 });
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'calculate-sum-for-query-in-selection') {
-    const query = msg.query as string;
-    const selectedNodes = figma.currentPage.selection.filter(node => node.type === 'STICKY' && node.text.characters.includes(query)) as StickyNode[];
+    const queryEnclosed = `[${msg.query}]`;
+    const regexNumbers = /\[\d+\]/g;
+
+    const selectedNodes = figma.currentPage.selection.filter(node => 
+      node.type === 'STICKY' && node.text.characters.includes(queryEnclosed)) as StickyNode[];
     let sum = 0;
 
-    console.log("selectedNodes", selectedNodes);
-    if(selectedNodes.length === 0) {
-      figma.notify("No stickies found with the query");
-      return;
-    }
-
     selectedNodes.forEach(node => {
-      const matches = node.text.characters.match(/\[\d+\]/g);
+      const matches = node.text.characters.match(regexNumbers);
       if (matches) {
         matches.forEach((match: string) => {
-          // 抽出した文字列から数字部分を取得し、整数に変換
-          const number = parseInt(match.match(/\d+/)![0], 10);
+          const number = parseInt(match.slice(1, -1), 10); 
           sum += number;
         });
       }
     });
 
-    // 計算結果をUIに送信
     figma.ui.postMessage({ type: 'sum-result', sum });
   }
 };
-
-
-figma.showUI(__html__, { width: 240, height: 100 });
-
