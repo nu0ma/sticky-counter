536cfe0dd6a13584b43dcefbb6d97c541f97dff0
nu0ma <igreen.re317v10@gmail.com>
2024-02-19 22:05:26 +0900

    f

--- code.ts
+++ code.ts
@@ -1,53 +1,33 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many shapes and connectors on the screen.
+figma.showUI(__html__, { width: 240, height: 140 });
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
 figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    const numberOfShapes = msg.count;
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfShapes; i++) {
-      const shape = figma.createShapeWithText();
-      // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-      shape.shapeType = 'ROUNDED_RECTANGLE'
-      shape.x = i * (shape.width + 200);
-      shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(shape);
-      nodes.push(shape);
-    };
+  if (msg.type === 'calculate-sum-for-query-in-selection') {
+    const query = msg.query as string;
+    const selectedNodes = figma.currentPage.selection.filter(node => node.type === 'STICKY' && node.text.characters.includes(query)) as StickyNode[];
+    let sum = 0;
 
-    for (let i = 0; i < (numberOfShapes - 1); i++) {
-      const connector = figma.createConnector();
-      connector.strokeWeight = 8
+    console.log("selectedNodes", selectedNodes);
+    if(selectedNodes.length === 0) {
+      figma.notify("No stickies found with the query");
+      return;
+    }
 
-      connector.connectorStart = {
-        endpointNodeId: nodes[i].id,
-        magnet: 'AUTO',
-      };
+    selectedNodes.forEach(node => {
+      const matches = node.text.characters.match(/\[\d+\]/g);
+      if (matches) {
+        matches.forEach((match: string) => {
+          // 抽出した文字列から数字部分を取得し、整数に変換
+          const number = parseInt(match.match(/\d+/)![0], 10);
+          sum += number;
+        });
+      }
+    });
 
-      connector.connectorEnd = {
-        endpointNodeId: nodes[i+1].id,
-        magnet: 'AUTO',
-      };
-    };
-
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    // 計算結果をUIに送信
+    figma.ui.postMessage({ type: 'sum-result', sum });
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
 };
+
+
+figma.showUI(__html__, { width: 240, height: 100 });
+
